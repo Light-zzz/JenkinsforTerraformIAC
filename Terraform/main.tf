cfc256ca4cbeff7db12ff69eb8599e5e29f172a8
Dipesh Vishwakarma <dipeshvishwa767@gmail.com>
2025-12-14 09:06:47 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,84 +1,54 @@
+
 # VPC
-############################
-resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  tags = {
-    Name = "terraform-vpc"
-  }
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = { Name = "Terraform-vpc" }
 }
 
 # Internet Gateway
-############################
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  tags = {
-    Name = "terraform-igw"
-  }
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "main-igw" }
 }
 
-# Public Subnet 1
-############################
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-north-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-1"
-  }
+# Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone_1a
+  tags = { Name = "public-subnet" }
 }
 
-# Public Subnet 2
-############################
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-north-1c"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-subnet-2"
-  }
+# Private Subnet
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.availability_zone_1b
+  tags = { Name = "private-subnet" }
 }
 
 # Route Table
-############################
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
-
+  vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.ssh_cidr
     gateway_id = aws_internet_gateway.igw.id
   }
-
-  tags = {
-    Name = "public-route-table"
-  }
+  tags = { Name = "public-route-table" }
 }
 
-# Route Table Associations
-############################
-resource "aws_route_table_association" "subnet1_association" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "subnet2_association" {
-  subnet_id      = aws_subnet.public_subnet_2.id
+# Route Table Association
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
 }
 
 # Security Group
-############################
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-sg"
-  description = "Allow SSH and HTTP"
-  vpc_id      = aws_vpc.main_vpc.id
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "jenkins-sg"
 
   ingress {
-    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -86,7 +56,13 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   ingress {
-    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -99,32 +75,30 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "ec2-security-group"
-  }
 }
 
-# EC2 Instance with User Data
-############################
-resource "aws_instance" "ansible_ec2" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.public_subnet_1.id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+# Master EC2
+resource "aws_instance" "master" {
+  ami                    = var.ami
+  instance_type          = var.instance_type_master
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
   associate_public_ip_address = true
-  key_name                    = var.key_name
-
-user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y software-properties-common
-              add-apt-repository --yes --update ppa:ansible/ansible
-              apt-get install -y ansible
-              ansible --version
-              EOF
-
-  tags = {
-    Name = "Ansible-Server"
-  }
+  key_name = var.key_name
+#  user_data = file("jenkins-user-data.sh")
+  tags = { Name = "Terraform-instance" }
 }
+
+# Slave EC2
+# resource "aws_instance" "slave" {
+# ami                    = var.ami
+#  instance_type          = var.instance_type_slave
+#  subnet_id              = aws_subnet.public.id
+#  vpc_security_group_ids = [aws_security_group.sg.id]
+#  associate_public_ip_address = true
+#  key_name = var.key_name
+#  user_data = file("Slave-user-data.sh")
+#  tags = { Name = "slave-instance" }
+#}
+
+#Completed......
