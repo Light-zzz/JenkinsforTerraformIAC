258a99747da4270a1b517a42fbbef9fd2748b3df
Dipesh Vishwakarma <dipeshvishwa767@gmail.com>
2025-12-16 09:47:59 +0530

    Update output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,25 +1,40 @@
-output "master_public_ip" {
+output "Prometheus_public_ip" {
 description = "Public IP of Jenkins master VM"
-value = aws_instance.master.public_ip
+value = aws_instance.Prometheus.public_ip
 }
 
-output "Master_Jenkins_url" {
+output "Prometheus" {
  description = "Jenkins URL on master EC2"
-value = "http://${aws_instance.master.public_ip}:8080"
+value = "http://${aws_instance.Prometheus.public_ip}:9090"
 }
 
-output "master_instance_id" {
-value = aws_instance.master.id
+output "Prometheus_instance_id" {
+value = aws_instance.Prometheus.id
 }
 
-#output "slave_instance_id" {
-#value = aws_instance.slave.id
-#}
-#
-#output "slave_instance_ip" {
-#  description = "Public IP of slave VM"
-#  value = aws_instance.slave.public_ip
-#}
+output "Grafana_instance_id" {
+value = aws_instance.Grafana.id
+}
+
+output "Grafana" {
+ description = "Jenkins URL on master EC2"
+value = "http://${aws_instance.Grafana.public_ip}:3000"
+}
+
+output "Grafana_instance_ip" {
+ description = "Public IP of slave VM"
+ value = aws_instance.Grafana.public_ip
+}
+
+output "Nginx" {
+    description = "Nginx URL"
+    value = "http://${aws_instance.Nginx-Server.public_ip}:8080"
+}
+
+output "Nginx-IP" {
+    description = "Nginx Public IP"
+  value = aws_instance.Nginx-Server.public_ip
+}
 
 output "vpc_id" {
 value = aws_vpc.main.id
